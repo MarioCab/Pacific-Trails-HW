4e64df64ccbf7f83e38d802b11458f8f6ad640d8
Mario Cabrera <yankeescab@gmail.com>
2024-10-16 20:42:06 -0400

    Added form validation for last name

--- validation.js
+++ validation.js
@@ -1,3 +1,9 @@
-function validateForm(){
-    
+function validateForm() {
+  let x = document.forms[0]["myLName"].value;
+
+  if (x == "") {
+    alert("Please fill out last name");
+    return false;
+  }
+  return true;
 }
